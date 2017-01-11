b1fd35a005339406c52531ffc7be1fa414da0dbd
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-01-11 17:03:03 -0500

    improvements

--- chat.rb
+++ chat.rb
@@ -13,7 +13,6 @@ $ip = gets.chomp
 
 $myip = %x[ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/']
 
-# THE ONE WITH HIGHER IP WILL BE THE SERVER
 $client = $myip.split('.')[3] > $ip.split('.')[3] ? false : true
 $cmd = $client ? "nc #{$ip} 5555 -w 120" : "nc -l 5555"
 
@@ -40,20 +39,11 @@ def get_msg()
   return pre_str + gets.chomp
 end
 
-
-def one()
-  puts send(get_msg)
-end
-
-def two()
+if $client then
   puts rec
 end
 
-if $client then
-  two()
-end
-
 while true do
-  one()
-  two()
+  puts send(get_msg)
+  puts rec
 end
